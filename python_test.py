8136dd575500ef337b014960e6877a8380acb6e5
Abhishek40 <abhishekraj144@gmail.com>
2019-02-24 12:43:23 +0530

    example

---
+++ python_test.py
@@ -0,0 +1,8 @@
+def chanheme(mylist):
+    mylist = [1,2,3,4]
+    print ("changes list : ", mylist)
+    return
+#function call
+mylist = [10,20,30,40]
+chanheme(mylist)
+print("before change : ", mylist)
